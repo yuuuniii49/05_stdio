6ffb6cac309c3db5195098811915f8e75c2ace44
yuuuniii49 <0409yoon@sookmyung.ac.kr>
2020-10-06 16:54:15 +0900

    06

--- main.c
+++ main.c
@@ -4,18 +4,27 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int c;	
-	int num=0;
+	int answer = 59;
+	int i;
+	int cnt = 0;
 	
-	printf("input a string : ");
-	
-	while ((c=getchar()) != '\n') {
-		if (c >= '0' && c <= '9') {
-			num++;
+	do {
+		printf("input a number : ");
+		scanf("%i", &i);
+		if ( i < answer ) {
+			printf("low!\n");
+			cnt++;
 		}
-	}
-	
-	printf("The number of digit is %i\n", num);	
+		else if ( i > answer ) {
+			printf("high!\n");
+			cnt++;
+		}
+		else {
+			cnt++;
+			printf("Congratulation! trials : %i\n", cnt);
+			break;
+		}
+	} while(i != answer);
 		
 	return 0;
 }
