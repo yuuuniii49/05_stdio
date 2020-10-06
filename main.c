8ff91995f1297b99d7024a9c76dac3f5c468fb98
yuuuniii49 <0409yoon@sookmyung.ac.kr>
2020-10-06 16:55:17 +0900

    04

--- main.c
+++ main.c
@@ -4,27 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int answer = 59;
-	int i;
-	int cnt = 0;
+	int n, i;
+	int sum = 0;
 	
-	do {
-		printf("input a number : ");
-		scanf("%i", &i);
-		if ( i < answer ) {
-			printf("low!\n");
-			cnt++;
-		}
-		else if ( i > answer ) {
-			printf("high!\n");
-			cnt++;
-		}
-		else {
-			cnt++;
-			printf("Congratulation! trials : %i\n", cnt);
-			break;
-		}
-	} while(i != answer);
+	printf("input a number : ");
+	scanf("%i", &n);
+	
+	for (i=1; i<=n; i++) {
+		sum += i;
+	}
+	
+	printf("The result is %i\n", sum);
 		
 	return 0;
 }
